20ce177a69c766f12a69e3f5c49a28bce11245a8
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-10 18:29:26 +0900

    사용자로부터 입력값을 받고 그 값으로 이름 context의 네임을 변경하기

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components/native';
 import {UserConsumer} from '../contexts/User';
 
@@ -7,11 +7,33 @@ const StyledText = styled.Text`
   font-weight: 600;
 `;
 
+const StyledInput = styled.TextInput`
+  border: 1px solid #111111;
+  font-size: 26px;
+  padding: 10px;
+  width: 100%;
+`;
+
 const User = () => {
+  const [text, setText] = useState('');
   return (
-    <UserConsumer>
-      {({name}) => <StyledText>Name: {name}</StyledText>}
-    </UserConsumer>
+    <>
+      <UserConsumer>
+        {({name}) => <StyledText>Name: {name}</StyledText>}
+      </UserConsumer>
+      <UserConsumer>
+        {({setName}) => (
+          <StyledInput
+            value={text}
+            onChangeText={setText}
+            onSubmitEditing={() => {
+              setName(text);
+              setText('');
+            }}
+          />
+        )}
+      </UserConsumer>
+    </>
   );
 };
 
