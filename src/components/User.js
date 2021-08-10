d8a627cbf3c44bd07179537a43e3d721b05c8db1
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-10 18:32:55 +0900

    useContext 훅을 사용해서 context를 이용하기 (이전코드보다 직관적이고 사용하기 편함)

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import styled from 'styled-components/native';
-import {UserConsumer} from '../contexts/User';
+import UserContext from '../contexts/User';
 
 const StyledText = styled.Text`
   font-size: 30px;
@@ -16,23 +16,19 @@ const StyledInput = styled.TextInput`
 
 const User = () => {
   const [text, setText] = useState('');
+  const {name, setName} = useContext(UserContext);
+
   return (
     <>
-      <UserConsumer>
-        {({name}) => <StyledText>Name: {name}</StyledText>}
-      </UserConsumer>
-      <UserConsumer>
-        {({setName}) => (
-          <StyledInput
-            value={text}
-            onChangeText={setText}
-            onSubmitEditing={() => {
-              setName(text);
-              setText('');
-            }}
-          />
-        )}
-      </UserConsumer>
+      <StyledText>Name: {name}</StyledText>
+      <StyledInput
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={() => {
+          setName(text);
+          setText('');
+        }}
+      />
     </>
   );
 };
